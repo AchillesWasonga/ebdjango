0b358324f76653ca6f040eb030c67ec5c66a103c
AchillesWasonga <aw6963@princeton.edu>
2025-03-28 19:32:02 -0400

    ready for deployment

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'skr_s5kf5$l10=)!$$g=s!(ga@efg-nfy2kaejt5@&ue-s+zvx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eb-django-app-dev.elasticbeanstalk.com']
 
 
 # Application definition
